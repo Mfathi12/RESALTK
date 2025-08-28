071b59cac614f7edd80f44184a57a6b447c33b46
Mfathi12 <mariamfathi17@gmail.com>
2025-08-28 03:11:45 +0300

    Your commit message

--- Resaltk/src/MiddleWare/Authorization.js
+++
@@ -1,8 +0,0 @@
-export const Authorization = (...role) => {
-    return (req, res, next) => {
-        if (role !== req.user.accountType) {
-            return next (new Error({ message: "Access denied, insufficient permissions" },{cause: 403 }));
-        }
-        next();
-    }
-}
--- Resaltk/src/Utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-export const asyncHandler=(controller)=> {
-    return async (req, res, next) => {
-        try {
-            await controller(req, res, next);
-        } catch (error) {
-            next(error);
-        }}
-}
