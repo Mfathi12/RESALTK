cf4b3a132a4e6708d8926868573efad20c579026
Mfathi12 <mariamfathi17@gmail.com>
2025-08-28 04:17:43 +0300

    Your commit message

--- src/MiddleWare/Authorization.js
+++ src/MiddleWare/Authorization.js
@@ -1,4 +1,12 @@
-export const Authorization = (...roles) => {
+export const Authorization = (...role) => {
+    return (req, res, next) => {
+        if (role !== req.user.accountType) {
+            return next (new Error({ message: "Access denied, insufficient permissions" },{cause: 403 }));
+        }
+        next();
+    }
+}
+/* export const Authorization = (...roles) => {
     return (req, res, next) => {
         if (!roles.includes(req.user.accountType)) {
             const error = new Error("Access denied, insufficient permissions");
@@ -8,3 +16,4 @@ export const Authorization = (...roles) => {
         next();
     }
 }
+ */
