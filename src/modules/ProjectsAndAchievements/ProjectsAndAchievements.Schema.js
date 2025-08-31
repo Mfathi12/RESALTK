097f22925170230e9350f8581af4d8f7c97b1ea0
Mfathi12 <mariamfathi17@gmail.com>
2025-08-31 17:26:49 +0300

    add

--- src/modules/ProjectsAndAchievements/ProjectsAndAchievements.Schema.js
+++ src/modules/ProjectsAndAchievements/ProjectsAndAchievements.Schema.js
@@ -3,8 +3,8 @@ import { isValidObjectId } from "../../MiddleWare/Validation.js";
 
 export const addProjectSchema = Joi.object({
     teamId:Joi.string().custom(isValidObjectId).required(),
-    title: Joi.string().required(),
-    description: Joi.string().required(),
+    projectTitle: Joi.string().required(),
+    projectDescription: Joi.string().required(),
     fieldOfResearch: Joi.string().required(),
     projectType: Joi.string().valid("Thesis", "Research Paper", "Experiment", "Case Study", "Other").required(),
 }).required();
