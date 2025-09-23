0a1e2cfa32b798453c8ded611053ddb433fe0251
Mfathi12 <mariamfathi17@gmail.com>
2025-09-23 11:28:43 +0300

    add

--- src/Utils/nodeMailer.js
+++ src/Utils/nodeMailer.js
@@ -1,23 +1,25 @@
 import nodemailer from "nodemailer";
-export const sendEmail = async ({ to, subject, html }) => {
-    //sender
-    const transporter = nodemailer.createTransport({
-        host: "localhost",
-        port: 587,
-        secure: true,
-        service: "Gmail",
-        auth: {
-            user: process.env.USER,
-            pass: process.env.PASS,
-        },
-    });
 
-    //reciver
-    const mailOptions = await transporter.sendMail({
-        from: `Resaltk Application  `,
-        to: to,
-        subject: subject,
-    });
-    if (mailOptions.rejected.length > 0) return false;
-    return true;
+export const sendEmail = async ({ to, subject, html }) => {
+  // sender
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS, // لازم App Password
+    },
+  });
+
+  // receiver
+  const mailOptions = {
+    from: `"Resaltk Application 🥼" <${process.env.USER}>`, // مهم يكون ايميل
+    to,
+    subject,
+    html, // لازم تبعتي محتوى
+  };
+
+  const info = await transporter.sendMail(mailOptions);
+
+  if (info.rejected.length > 0) return false;
+  return true;
 };
